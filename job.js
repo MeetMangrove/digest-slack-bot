1121f36472487a0a3f00770fc30a799e66e93ee5
Adrien Joly <adrien@whyd.com>
2016-08-22 17:12:12 +0200

    job is working and tagging messages as broadcasted!

--- job.js
+++ job.js
@@ -23,25 +23,34 @@ console.log('find...');
 model.Message.find({ broadcasted: { $ne: true } }).sort({ votes: -1 }).limit(LIMIT).exec(function(err, msgs){
   console.log('find=>', err || msgs);
   fetchMessageContent(msgs, function(err, messages){
-    messages.forEach(function(msg){
-      // syntax: <nb_votes> :balloon: <URL>
-      var url = 'http://' + config.SLACK_NAME + '.slack.com/archives/' + msg.channelName + '/p' + msg.id.replace('.', '');
-      var digestLine = msg.votes + ' :' + config.REACTION_NAME + ': ' + url;
-      // display in the #gen-bot channel
-      //config.bot.postMessageToChannel(config.DIGEST_CHANNEL, digestLine);
-      console.log(digestLine); // TODO
-    });
 
-    // TODO: send digest to admin, then allow admin to edit it before sharing it publicly
-    // config.bot.postMessageToUser(config.ADMIN_USERNAME, digest, { 'slackbot': true, icon_emoji: ':cat:' });
-    // will post the user, from "Gem Bot" (instead of slack's slackbot channel)
+    if (messages.length === 0) {
+      config.bot.postMessageToChannel(config.DIGEST_CHANNEL, 'Sorry, no new messages with ' + config.REACTION_NAME + ' this time... :\'-(', {
+        unfurl_links: true
+      });
+    } else {
+      messages.forEach(function(msg){
+        // syntax: <nb_votes> :balloon: <URL>
+        var url = 'https://' + config.SLACK_NAME + '.slack.com/archives/' + msg.channelName + '/p' + msg.id.replace('.', '');
+        var digestLine = msg.votes + ' :' + config.REACTION_NAME + ': ' + url;
+        console.log(digestLine); // TODO
+        // display in the #gen-bot channel
+        config.bot.postMessageToChannel(config.DIGEST_CHANNEL, digestLine, {
+          unfurl_links: true
+        });
+      });
 
-    // mark messages
-    const ids = messages.map(m => m.id);
-    /*
-    model.Message.update({ id: { $in : ids } }, { $set: { broadcasted: true } }, { multi: true }, (err, res) => {
-      err && console.error(err);
-    });
-    */
+      // TODO: send digest to admin, then allow admin to edit it before sharing it publicly
+      // config.bot.postMessageToUser(config.ADMIN_USERNAME, digest, { 'slackbot': true, icon_emoji: ':cat:' });
+      // will post the user, from "Gem Bot" (instead of slack's slackbot channel)
+
+      // mark messages
+      const ids = messages.map(m => m.id);
+
+      // prevent a message from being digested twice
+      model.Message.update({ id: { $in : ids } }, { $set: { broadcasted: true } }, { multi: true }, (err, res) => {
+        err && console.error(err);
+      });
+    }
   });
 });
